5fffe4215a29db797dd2127babfc86553b963736
diksha <rajdiksha91@gmail.com>
2024-04-02 02:37:51 +0530

    set up

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+// import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ButtonGradient />
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login" /> Something <Button />
+      </div>
+    </>
+  );
+};
+
+export default App;
