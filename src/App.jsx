39a03f2b66edd8eaae9533b9e96d97e0d14f2fd4
diksha <rajdiksha91@gmail.com>
2024-04-03 12:48:18 +0530

    button component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ const App = () => {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <ButtonGradient />
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login" /> Something <Button />
+        <Button className="mt-10" href="#login">
+          {" "}
+          Something{" "}
+        </Button>
       </div>
     </>
   );
