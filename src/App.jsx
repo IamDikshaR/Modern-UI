203170a906deef4bd1a2a04c66864f945ad19779
diksha <rajdiksha91@gmail.com>
2024-04-04 02:41:12 +0530

    header skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 // import "./App.css";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <ButtonGradient />
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          {" "}
-          Something{" "}
-        </Button>
+        {/* <Button className="mt-10" href="#login">
+          Testing
+        </Button> */}
+        <Router>
+          <Header />
+        </Router>
       </div>
     </>
   );
